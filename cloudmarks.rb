0941924b6fd0dd5ba58404644f06f476dac6cd4c
Clint Troxel <clint@ctro.net>
2009-11-10 20:12:40 -0800

    datamapper, erb

--- cloudmarks.rb
+++ cloudmarks.rb
@@ -1,7 +1,24 @@
 require 'rubygems'
 require 'sinatra'
+require 'dm-core'
 
+# DataMapper setup
+# http://datamapper.org/getting-started.html
+DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/cloudmarks.sqlite3")
+
+class Mark
+  include DataMapper::Resource
+
+  property :id,         Serial
+  property :title,      String
+  property :uri,        String
+  property :created_at, DateTime
+end
+
+DataMapper.auto_upgrade!
+
+# index
 get '/' do
-  "Hello World!"
+  erb :index
 end
 
