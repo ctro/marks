25b1235b65b9d8019d2f6908aad27f807ed1bf29
Clint Troxel <clint@ctro.net>
2009-11-10 20:52:22 -0800

    User model

--- cloudmarks.rb
+++ cloudmarks.rb
@@ -6,12 +6,27 @@ require 'dm-core'
 # http://datamapper.org/getting-started.html
 DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/cloudmarks.sqlite3")
 
+class User
+  include DataMapper::Resource
+  
+  has n :marks
+  
+  property :id,         Serial
+  property :name,       String
+  property :created_at, DateTime
+end
+
 class Mark
   include DataMapper::Resource
+  
+  has 1 :user
 
   property :id,         Serial
-  property :title,      String
-  property :uri,        String
+  property :user_id,    Serial
+  property :title,      String, :nullable => false
+  property :uri,        String, :nullable => false
+  property :x,          Serial, :default => 33
+  property :y,          Serial, :default => 33
   property :created_at, DateTime
 end
 
